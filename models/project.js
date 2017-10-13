3ddc21226e06e774f2152416af922524d24c4ae0
Sophia Chow <Sophia.t.chow@gmail.com>
2017-10-13 16:02:19 -0500

    created models

---
+++ models/project.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const GoalsSchema = new mongoose.Schema({
+  description: String,
+  voteToActive: Boolean,
+  voteToComplete: Boolean
+});
+
+const ProjectSchema = new mongoose.Schema({
+  name: String,
+  contributors: [{
+    type: mongoose.Schema.ObjectId,
+    ref: "User"
+  }],
+  description: String,
+  completed: Boolean,
+  goals: [GoalsSchema],
+  chat: String //TODO: figure out how to create a chat schema
+});
+
+const Goal = mongoose.model('Goal', GoalsSchema);
+const Project = mongoose.model('Project', ProjectSchema);
+
+module.exports = {
+  GoalsSchema: GoalsSchema,
+  ProjectSchema: ProjectSchema
+};
+
